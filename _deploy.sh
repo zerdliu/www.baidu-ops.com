e5f04c37c8f3f48f7f24f5c67eeae83f1185ecdb
zerdliu <supersugarcane@126.com>
2012-08-03 08:43:40 +0800

    replace _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function data_upload() {
+function data_upload_ftp() {
 
   ftp -inv 174.120.207.225 << EOF
 
@@ -17,22 +17,25 @@ EOF
 
 }
 
-#!/bin/bash    
-HOST="174.120.207.225"
-USER="baiduops"
-PASS="3bEL17i3ft"
-LCD="_site"
-RCD="public_html/"
-lftp -c "
-set ssl:verify-certificate no
-set ftp:list-options -a;
-open ftp://$USER:$PASS@$HOST; 
-lcd $LCD;
-cd $RCD;
-mirror --reverse \
-       --delete \
-       --verbose \
-       --exclude-glob a-dir-to-exclude/ \
-       --exclude-glob a-file-to-exclude \
-       --exclude-glob a-file-group-to-exclude* \
-       --exclude-glob other-files-to-esclude"
+function data_upload_lftp() {
+  HOST="174.120.207.225"
+  USER="baiduops"
+  PASS="3bEL17i3ft"
+  LCD="_site"
+  RCD="public_html/"
+  lftp -c "
+  set ssl:verify-certificate no
+  set ftp:list-options -a;
+  open ftp://$USER:$PASS@$HOST; 
+  lcd $LCD;
+  cd $RCD;
+  mirror --reverse \
+         --delete \
+         --verbose \
+         --exclude-glob a-dir-to-exclude/ \
+         --exclude-glob a-file-to-exclude \
+         --exclude-glob a-file-group-to-exclude* \
+         --exclude-glob other-files-to-esclude"
+}
+
+data_upload_lftp
